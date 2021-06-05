fee132a4cba8d9269b6aab12ac40f5a818f8917a
keaising <keaising@gmail.com>
2021-06-05 15:17:29 +0800

    add nvm

--- zsh/.config/zsh/tools.zsh
+++ zsh/.config/zsh/tools.zsh
@@ -30,6 +30,13 @@ fi
 export GITHUB_LOCATION="$HOME/code/github.com"
 export LOCAL_BIN="$HOME/.local/bin/"
 
+
+if [ -d "$HOME/.config/nvm" ]; then
+	export NVM_DIR="$HOME/.config/nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
 # ===================================================================
 # install tools exist
 # ===================================================================
