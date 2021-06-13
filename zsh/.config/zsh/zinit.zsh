04b45c6f180e1fe7862ee74d6c7b1cfc3dca7ae2
keaising <keaising@gmail.com>
2021-06-13 12:39:45 +0800

    remove extra completions

--- zsh/.config/zsh/zinit.zsh
+++ zsh/.config/zsh/zinit.zsh
@@ -39,8 +39,10 @@ zinit light mafredri/zsh-async
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-syntax-highlighting
 
-zinit ice wait lucid atload"zicompinit"
-zinit light zsh-users/zsh-completions
+# If completion is needed, de-comment lines below.
+# this will add 200ms loading time.
+# zinit ice wait lucid atload"zicompinit"
+# zinit light zsh-users/zsh-completions
 
 # ==================================================================
 # update zinit, only run update or first install
@@ -51,41 +53,5 @@ zinit light zsh-users/zsh-completions
 # plugin config
 # ==================================================================
 
-# # theme
-# autoload -U promptinit; promptinit
-# # prompt -p pure
-# export PURE_PROMPT_SYMBOL='Δ'
-# export GIT_TERMINAL_PROMPT=1
-# zstyle :prompt:pure:git:stash show yes
-# # prompt_pure_state[username]=
-
 # ZSH_AUTOSUGGEST
 ZSH_AUTOSUGGEST_USE_ASYNC=1
-
-
-# # ZSH_SYNTAX_HIGHLIGHT color definition
-# ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
-# typeset -A ZSH_HIGHLIGHT_STYLES
-# ZSH_HIGHLIGHT_STYLES[command]=fg=white,bold
-# ZSH_HIGHLIGHT_STYLES[alias]='fg=magenta,bold'
-# ZSH_HIGHLIGHT_STYLES[default]=none
-# ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=009
-# ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=009,standout
-# ZSH_HIGHLIGHT_STYLES[alias]=fg=cyan,bold
-# ZSH_HIGHLIGHT_STYLES[builtin]=fg=cyan,bold
-# ZSH_HIGHLIGHT_STYLES[function]=fg=cyan,bold
-# ZSH_HIGHLIGHT_STYLES[command]=fg=white,bold
-# ZSH_HIGHLIGHT_STYLES[precommand]=fg=white,underline
-# ZSH_HIGHLIGHT_STYLES[commandseparator]=none
-# ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=009
-# ZSH_HIGHLIGHT_STYLES[path]=fg=214,underline
-# ZSH_HIGHLIGHT_STYLES[globbing]=fg=063
-# ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=white,underline
-# ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=none
-# ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=none
-# ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=none
-# ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=063
-# ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=063
-# ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=009
-# ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=009
-# ZSH_HIGHLIGHT_STYLES[assign]=none
