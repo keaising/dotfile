05cae80c2c10c705cedfd66aae925393b68d0b80
keaising <keaising@gmail.com>
2021-06-14 15:59:26 +0800

    edit zsh commands in vim

--- zsh/.config/zsh/keymap.zsh
+++ zsh/.config/zsh/keymap.zsh
@@ -1,5 +1,13 @@
-bindkey -s '\ee' 'nvim .\n'
+# edit in vim
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '^x^e' edit-command-line
+bindkey '\ei' edit-command-line
+# bindkey -M vicmd v edit-command-line
 
+
+
+# move cursor
 bindkey '\eH' backward-char
 bindkey '\eL' forward-char
 bindkey '\eJ' down-line-or-history
@@ -9,14 +17,21 @@ bindkey '\el' forward-word
 bindkey '\ej' beginning-of-line
 bindkey '\ek' end-of-line
 
-bindkey -s '\eo' 'cd ..\n'
-bindkey -s '\e;' 'll\n'
-
 bindkey '\e[1;3D' backward-word
 bindkey '\e[1;3C' forward-word
 bindkey '\e[1;3A' beginning-of-line
 bindkey '\e[1;3B' end-of-line
 
+
+
+# shortcuts
+bindkey -s '\ee' 'nvim .\n'
+bindkey -s '\eo' 'cd ..\n'
+bindkey -s '\e;' 'll\n'
+
+
+
+# auto suggestion
 bindkey '^\' autosuggest-excute
 bindkey '^n' autosuggest-accept
 
