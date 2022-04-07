0efec309309ddd99742bd40dec7ddd2de95de0d5
keaising <keaising@gmail.com>
2022-04-07 11:49:23 +0800

    update

--- nvim/.config/nvim/lua/plugin.lua
+++ nvim/.config/nvim/lua/plugin.lua
@@ -91,7 +91,9 @@ return function(packer)
 					custom = { ".git" },
 				},
 			})
-			vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+			vim.cmd(
+				"autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+			)
 		end,
 	})
 
@@ -111,7 +113,9 @@ return function(packer)
 		config = function()
 			-- vim.cmd 'set termguicolors'
 			vim.cmd("color gruvbox-material")
-			vim.cmd("hi LspSignatureActiveParameter guifg=NONE ctermfg=NONE guibg=#1d1f21 ctermbg=53 gui=Bold,underline,Italic cterm=Bold,underline,Italic guisp=#fbec9f")
+			vim.cmd(
+				"hi LspSignatureActiveParameter guifg=NONE ctermfg=NONE guibg=#1d1f21 ctermbg=53 gui=Bold,underline,Italic cterm=Bold,underline,Italic guisp=#fbec9f"
+			)
 		end,
 	})
 
@@ -127,11 +131,75 @@ return function(packer)
 	packer({
 		"f-person/git-blame.nvim",
 		config = function()
-			vim.g.gitblame_message_template = '       <author> <date> <summary>'
-			vim.g.gitblame_date_format = '%Y-%m-%d %H:%M'
-		end
+			vim.g.gitblame_message_template = "       <author> <date> <summary>"
+			vim.g.gitblame_date_format = "%Y-%m-%d %H:%M"
+		end,
 	})
 
+	packer({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+				position = "bottom", -- position of the list can be: bottom, top, left, right
+				height = 10, -- height of the trouble list when position is top or bottom
+				width = 50, -- width of the list when position is left or right
+				icons = true, -- use devicons for filenames
+				mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+				fold_open = "", -- icon used for open folds
+				fold_closed = "", -- icon used for closed folds
+				group = true, -- group results by file
+				padding = true, -- add an extra new line on top of the list
+				action_keys = { -- key mappings for actions in the trouble list
+					-- map to {} to remove a mapping, for example:
+					-- close = {},
+					close = "q", -- close the list
+					cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+					refresh = "r", -- manually refresh
+					jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
+					open_split = { "<c-x>" }, -- open buffer in new split
+					open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+					open_tab = { "<c-t>" }, -- open buffer in new tab
+					jump_close = { "o" }, -- jump to the diagnostic and close the list
+					toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+					toggle_preview = "P", -- toggle auto_preview
+					hover = "K", -- opens a small popup with the full multiline message
+					preview = "p", -- preview the diagnostic location
+					close_folds = { "zM", "zm" }, -- close all folds
+					open_folds = { "zR", "zr" }, -- open all folds
+					toggle_fold = { "zA", "za" }, -- toggle fold of current file
+					previous = "k", -- preview item
+					next = "j", -- next item
+				},
+				indent_lines = true, -- add an indent guide below the fold icons
+				auto_open = false, -- automatically open the list when you have diagnostics
+				auto_close = false, -- automatically close the list when you have no diagnostics
+				auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+				auto_fold = false, -- automatically fold a file trouble list at creation
+				auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
+				signs = {
+					-- icons / text used for a diagnostic
+					error = "",
+					warning = "",
+					hint = "",
+					information = "",
+					other = "﫠",
+				},
+				use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+			})
+
+			local util = require("util")
+			util.noremap("n", "<leader>xx", "<cmd>TroubleToggle<cr>")
+			util.noremap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>")
+			util.noremap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>")
+			util.noremap("n", "<leader>xl", "<cmd>Trouble loclist<cr>")
+			util.noremap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>")
+			util.noremap("n", "gR", "<cmd>Trouble lsp_references<cr>")
+		end,
+	})
 	packer({
 		"nvim-lualine/lualine.nvim",
 		requires = {
@@ -177,12 +245,17 @@ return function(packer)
 			local util = require("util")
 			util.noremap("n", "<m-{>", ":BufferLineCyclePrev<CR>")
 			util.noremap("n", "<m-}>", ":BufferLineCycleNext<CR>")
+			util.noremap("n", "<m-<>", ":BufferLineMovePrev<CR>")
+			util.noremap("n", "<m->>", ":BufferLineMoveNext<CR>")
 			util.noremap("n", "<m-w>", ":bd<CR>")
 			util.noremap("n", "<m-e>", ":BufferLinePick<CR>")
 			require("bufferline").setup({
 				options = {
 					-- mode = 'tabs'
 					numbers = "ordinal",
+					show_buffer_close_icons = false,
+					show_close_icon = false,
+					show_tab_indicators = false,
 				},
 			})
 		end,
