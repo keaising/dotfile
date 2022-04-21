fd08756eeab18b68cd605a96b60b44973a30e601
keaising <keaising@gmail.com>
2022-04-21 16:07:29 +0800

    update

--- nvim/.config/nvim/lua/completion.lua
+++ nvim/.config/nvim/lua/completion.lua
@@ -29,8 +29,8 @@ function M.init(packer)
 				mapping = {
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-j>"] = cmp.mapping.select_next_item(),
-					["<C-k>"] = cmp.mapping.select_prev_item(),
+					-- ["<C-j>"] = cmp.mapping.select_next_item(),
+					-- ["<C-k>"] = cmp.mapping.select_prev_item(),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.close(),
 					-- ["<CR>"] = cmp.mapping.confirm({
