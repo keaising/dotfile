fcfcb5a1ba975059035cdfef0729fcf98c913566
keaising <keaising@gmail.com>
2022-07-22 14:33:20 +0800

    preparation for im-select

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -3,3 +3,9 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
     vim.cmd "hi link illuminatedWord LspReferenceText"
   end,
 })
+
+vim.api.nvim_create_autocmd({ "InsertLeave "}, {
+	callback = function ()
+		vim.cmd [[ :echo "leave insert" ]]
+	end
+})
