5365a47daa6bd30f589fa6e840d56984b5948158
keaising <keaising@gmail.com>
2022-07-25 15:29:32 +0800

    update auto switch im

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -4,8 +4,23 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "InsertLeave "}, {
+vim.api.nvim_create_autocmd({ "InsertEnter "}, {
 	callback = function ()
-		vim.cmd [[ :echo "leave insert" ]]
+		local current = vim.fn.system{ 'im-select' }
+		if vim.g["taiga_current_im_select"] ~= current then
+			-- I have no idea why this don't work
+			vim.fn.system{ 'im-select', vim.g["taiga_current_im_select"] }
+		end
 	end
 })
+
+vim.api.nvim_create_autocmd({ "InsertLeave "}, {
+	callback = function ()
+		local current = vim.fn.system{ 'im-select' }
+		if current ~= "com.apple.keylayout.ABC" then
+			vim.api.nvim_set_var('taiga_current_im_select', current)
+			vim.fn.system{ 'im-select', 'com.apple.keylayout.ABC' }
+		end
+	end
+})
+
