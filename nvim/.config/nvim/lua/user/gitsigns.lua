9ae64bb97486cc9123e1e99cc280b563716d827e
keaising <keaising@gmail.com>
2022-07-25 17:18:13 +0800

    replace git blame tool

--- nvim/.config/nvim/lua/user/gitsigns.lua
+++ nvim/.config/nvim/lua/user/gitsigns.lua
@@ -21,14 +21,15 @@ gitsigns.setup {
 		follow_files = true
 	},
 	attach_to_untracked = true,
-	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	-- this blame info will cover my code, annoying!
+	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
-		virt_text = true,
+		virt_text = false,
 		virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
 		delay = 300,
-		ignore_whitespace = false,
+		ignore_whitespace = true,
 	},
-	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+	current_line_blame_formatter = '<author> <author_time:%Y-%m-%d>',
 	sign_priority = 0,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
