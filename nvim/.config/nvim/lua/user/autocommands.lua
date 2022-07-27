12d725a3d4e5e88706cf169e8605311d095f0e40
keaising <keaising@gmail.com>
2022-07-27 12:25:30 +0800

    finish auto switch im

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -8,26 +8,28 @@ local function all_trim(s)
    return s:match( "^%s*(.-)%s*$" )
 end
 
-local im_cn = "im.rime.inputmethod.Squirrel.Rime"
-local im_en = "com.apple.keylayout.ABC"
+local default_im_select = vim.g['im_select_default_im_select']
+if default_im_select == nil or default_im_select == "" then
+	default_im_select = "com.apple.keylayout.ABC"
+end
 
 vim.api.nvim_create_autocmd({ "InsertEnter "}, {
 	callback = function ()
-		local current = all_trim(vim.fn.system{ 'im-select' })
-		if current ~= vim.g["taiga_current_im_select"] then
-			if vim.g["taiga_current_im_select"] == im_cn then
-				vim.fn.system{ 'im-select', im_cn }
-			end
+		local current_select = all_trim(vim.fn.system{ 'im-select' })
+		local save = vim.g["im_select_current_im_select"]
+		if current_select ~= save then
+			vim.fn.system{ 'im-select', save }
 		end
 	end
 })
 
 vim.api.nvim_create_autocmd({ "InsertLeave "}, {
 	callback = function ()
-		local current = all_trim(vim.fn.system{ 'im-select' })
-		vim.api.nvim_set_var('taiga_current_im_select', current)
-		if current ~= im_en then
-			vim.fn.system{ 'im-select', im_en }
+		local current_select = all_trim(vim.fn.system{ 'im-select' })
+		vim.api.nvim_set_var('im_select_current_im_select', current_select)
+
+		if current_select ~= default_im_select then
+			vim.fn.system{ 'im-select', default_im_select }
 		end
 	end
 })
