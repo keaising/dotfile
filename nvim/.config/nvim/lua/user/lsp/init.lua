eb97c8a6c19bffda9ae2bd5e8d255085524fc43b
keaising <keaising@gmail.com>
2022-07-30 11:36:46 +0800

    remove useless lsp installer config

--- nvim/.config/nvim/lua/user/lsp/init.lua
+++ nvim/.config/nvim/lua/user/lsp/init.lua
@@ -1,7 +1,47 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
+local lspconfig_ok, _ = pcall(require, "lspconfig")
+if not lspconfig_ok then
 	return
 end
 
-require("user.lsp.configs")
-require("user.lsp.handlers").setup()
+
+local servers = {
+	"jsonls",
+	"sumneko_lua",
+	"gopls",
+	"bashls",
+	"dockerls",
+	"golangci_lint_ls",
+	"pyright",
+	-- "grammerly",
+	"tsserver",
+	"vimls",
+}
+
+-- lsp installer
+local installer_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not installer_ok then
+	return
+end
+
+lsp_installer.setup({
+	ensure_installed = servers,
+})
+
+
+-- setup lsp for lspconfig
+local lspconfig = require("lspconfig")
+local lsphandler = require("user.lsp.handlers")
+
+for _, server in pairs(servers) do
+	local opts = {
+		on_attach = lsphandler.on_attach,
+		capabilities = lsphandler.capabilities,
+	}
+	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+	end
+	lspconfig[server].setup(opts)
+end
+
+lsphandler.setup()
