599a860b95b0e630c954581d53d7d697facb2567
keaising <keaising@gmail.com>
2022-08-14 23:13:23 +0800

    update

--- nvim/.config/nvim/lua/user/go.lua
+++ nvim/.config/nvim/lua/user/go.lua
@@ -17,5 +17,6 @@ go.setup({
 vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
 
 vim.api.nvim_set_keymap("n", "ggt", ":GoAddTest<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gj", ":GoAddTag json<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gjb", ":GoAddTag json,bson<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gmt", ":GoModTidy<CR>", { noremap = true, silent = false })
