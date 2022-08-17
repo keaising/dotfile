a1532d688f67a83995bc4ac3df326543c5fb78ee
keaising <keaising@gmail.com>
2022-08-18 00:25:17 +0800

    add intend blankline

--- nvim/.config/nvim/lua/user/setups.lua
+++ nvim/.config/nvim/lua/user/setups.lua
@@ -13,7 +13,6 @@ local plugin_list = {
 			border = "none",
 		},
 	},
-
 	bufferline = {
 		options = {
 			numbers = "ordinal",
@@ -48,10 +47,31 @@ local plugin_list = {
 			},
 		},
 	},
-	['treesitter-context'] = {
-		mode = 'topline',
+	["treesitter-context"] = {
+		mode = "topline",
+	},
+	hop = {},
+	indent_blankline = {
+		space_char_blankline = " ",
+		char_highlight_list = {
+			"IndentBlanklineIndent1",
+			"IndentBlanklineIndent2",
+			"IndentBlanklineIndent3",
+			"IndentBlanklineIndent4",
+			"IndentBlanklineIndent5",
+			"IndentBlanklineIndent6",
+		},
 	},
 }
+
+vim.opt.list = true
+vim.g.indent_blankline_filetype = {'yml', 'yaml', 'json'}
+vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
 for p, opt in pairs(plugin_list) do
 	local status_ok, plugin = pcall(require, p)
