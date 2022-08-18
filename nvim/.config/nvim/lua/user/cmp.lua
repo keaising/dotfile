6f818e102cb90bb4d15c0738b7d460196341dab6
keaising <keaising@gmail.com>
2022-08-18 11:43:54 +0800

    add cmp emoji

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -1,42 +1,42 @@
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-    return
+	return
 end
 
-require("cmp_nvim_ultisnips").setup {}
+require("cmp_nvim_ultisnips").setup({})
 local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
 
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-    Text = "",
-    Method = "m",
-    Function = "",
-    Constructor = "",
-    Field = "",
-    Variable = "",
-    Class = "",
-    Interface = "",
-    Module = "",
-    Property = "",
-    Unit = "",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "",
-    Event = "",
-    Operator = "",
-    TypeParameter = "",
+	Text = "",
+	Method = "m",
+	Function = "",
+	Constructor = "",
+	Field = "",
+	Variable = "",
+	Class = "",
+	Interface = "",
+	Module = "",
+	Property = "",
+	Unit = "",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "",
+	Event = "",
+	Operator = "",
+	TypeParameter = "",
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
-cmp.setup {
+cmp.setup({
 	snippet = {
 		expand = function(args)
 			vim.fn["UltiSnips#Anon"](args.body)
@@ -51,19 +51,19 @@ cmp.setup {
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<C-e>"] = cmp.mapping {
+		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
-		},
+		}),
 		-- Accept currently selected item. If none selected, `select` first item.
 		-- Set `select` to `false` to only confirm explicitly selected items.
-		["<CR>"] = cmp.mapping.confirm { select = true },
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping(function(fallback)
-				cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
-			end, { "i", "s" }),
+			cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+		end, { "i", "s" }),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
-				cmp_ultisnips_mappings.jump_backwards(fallback)
-			end, { "i", "s" }),
+			cmp_ultisnips_mappings.jump_backwards(fallback)
+		end, { "i", "s" }),
 	},
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
@@ -71,24 +71,25 @@ cmp.setup {
 			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
 			vim_item.menu = ({
 				ultisnips = "[Snippet]",
-				nvim_lsp  = "[LSP]",
-				nvim_lua  = "[NVIM_LUA]",
-				buffer    = "[Buffer]",
-				path      = "[Path]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[NVIM_LUA]",
+				buffer = "[Buffer]",
+				path = "[Path]",
 			})[entry.source.name]
 			return vim_item
 		end,
 	},
 	sources = {
 		{ name = "ultisnips", group_index = 1 },
-		{ name = "nvim_lsp",  group_index = 1 },
-		{ name = "path",      group_index = 1 },
-		{ name = "buffer",    group_index = 2 },
-		{ name = "nvim_lua",  group_index = 2 },
+		{ name = "nvim_lsp", group_index = 1 },
+		{ name = "path", group_index = 1 },
+		{ name = "emoji", group_index = 1 },
+		{ name = "buffer", group_index = 2 },
+		{ name = "nvim_lua", group_index = 2 },
 	},
 	preselect = cmp.PreselectMode.None,
 	experimental = {
 		ghost_text = false,
 		native_menu = false,
 	},
-}
+})
