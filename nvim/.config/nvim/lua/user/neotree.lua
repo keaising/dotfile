cb61a72a2166e0c361b274587853aa7fa71cedee
keaising <keaising@gmail.com>
2023-02-08 17:16:36 +0800

    update neotree

--- nvim/.config/nvim/lua/user/neotree.lua
+++ nvim/.config/nvim/lua/user/neotree.lua
@@ -131,7 +131,7 @@ neotree.setup({
 		},
 		follow_current_file = false, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
-		group_empty_dirs = true, -- when true, empty folders will be grouped together
+		group_empty_dirs = false, -- when true, empty folders will be grouped together
 		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
 		-- in whatever position is specified in window.position
 		-- "open_current",  -- netrw disabled, opening a directory opens within the
@@ -157,7 +157,7 @@ neotree.setup({
 	buffers = {
 		follow_current_file = true, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
-		group_empty_dirs = true, -- when true, empty folders will be grouped together
+		group_empty_dirs = false, -- when true, empty folders will be grouped together
 		show_unloaded = true,
 		window = {
 			mappings = {
