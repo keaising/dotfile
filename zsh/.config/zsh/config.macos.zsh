6b35abfe61c410a24835aa91dd47ee09c6543e4e
keaising <keaising@gmail.com>
2023-02-10 15:39:08 +0800

    add alias for tailscale

--- zsh/.config/zsh/config.macos.zsh
+++ zsh/.config/zsh/config.macos.zsh
@@ -7,17 +7,5 @@ export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
 alias i='brew install'
 alias ic='brew cask install'
 alias dnsm='sudo brew services restart dnsmasq'
-
-
-# gcloud
-# export CLOUDSDK_PYTHON=python2
-# # updates PATH for the Google Cloud SDK.
-# if [ -f '/Users/taiga/code/app/gcloud/path.zsh.inc' ]; 
-# then 
-# 	. '/Users/taiga/code/app/gcloud/path.zsh.inc'; 
-# fi
-# # enables shell command completion for gcloud.
-# if [ -f '/Users/taiga/code/app/gcloud/completion.zsh.inc' ]; 
-# then 
-# 	. '/Users/taiga/code/app/gcloud/completion.zsh.inc'; 
-# fi
+alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+alias ts="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
--- zsh/.config/zsh/config.wsl.zsh
+++ zsh/.config/zsh/config.wsl.zsh
@@ -1,7 +1,1 @@
-# alias exp='explorer.exe'
-
-# # color list: https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
-# zstyle :prompt:pure:path color 241
-# zstyle :prompt:pure:git:branch color 031
-# zstyle :prompt:pure:prompt:error color 160
-# zstyle :prompt:pure:prompt:success color 031
+alias ts='tailscale'
