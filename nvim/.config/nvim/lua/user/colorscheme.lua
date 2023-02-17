2851bd00a857acaa1664c3d441f91d7de1f4432e
keaising <keaising@gmail.com>
2023-02-17 23:57:47 +0800

    revert to grubox

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -5,8 +5,8 @@
 --         let g:sonokai_style = 'shusia'
 --         let g:sonokai_better_performance = 1
 -- ]]
--- local colorscheme = "gruvbox-material"
-local colorscheme = "xcodedark"
+local colorscheme = "gruvbox-material"
+-- local colorscheme = "xcodedark"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
