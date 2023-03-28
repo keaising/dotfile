74a49439d858e3cc223c8c6e250b56b1d46a70f1
keaising <keaising@gmail.com>
2023-03-28 14:55:30 +0800

    update indent line

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -56,26 +56,13 @@ return {
     lazy = false,
     config = function()
       vim.opt.termguicolors = true
-      vim.opt.list = true
+      -- vim.opt.list = true
       vim.opt.listchars:append("space:⋅")
       vim.opt.listchars:append("eol:↴")
-      vim.g.indent_blankline_filetype = { "yml", "yaml", "json" }
-      vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+      vim.g.indent_blankline_filetype = { "yml", "yaml", "json", "lua" }
       require("indent_blankline").setup({
+        show_end_of_line = true,
         space_char_blankline = " ",
-        char_highlight_list = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-          "IndentBlanklineIndent3",
-          "IndentBlanklineIndent4",
-          "IndentBlanklineIndent5",
-          "IndentBlanklineIndent6",
-        },
       })
     end,
   },
