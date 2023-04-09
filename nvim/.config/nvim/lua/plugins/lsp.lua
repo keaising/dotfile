c0cb326109db820e8d8f4520e26d6822b440f9a0
keaising <keaising@gmail.com>
2023-04-09 13:36:07 +0800

    update tmux new-window process

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -210,6 +210,7 @@ return {
                     "black",
                     "cspell",
                     "jq",
+                    "prettier",
                     "shfmt",
                     "stylua",
                 },
