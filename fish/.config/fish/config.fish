02b4c0167d742b2791fb0172e693c9c3f27f4df4
keaising <keaising@gmail.com>
2023-05-14 11:22:02 +0800

    update extract

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -253,8 +253,8 @@ function px3
     setpx "10.10.43.3:1080"
 end
 
-function px6
-    setpx "10.10.43.6:1080"
+function px5
+    setpx "10.10.43.5:1080"
 end
 
 function px127
@@ -316,41 +316,53 @@ function note
     end
 end
 
+
 function extract
-    if test -f $argv[1]
-        switch $argv[1]
-            case *.tar.bz2
-                tar xjf $argv[1]
-            case *.tar.gz
-                tar xzf $argv[1]
-            case *.tar.xz
-                tar xf $argv[1]
-            case *.bz2
-                bunzip2 $argv[1]
-            case *.rar
-                unrar e $argv[1]
-            case *.gz
-                gunzip $argv[1]
-            case *.tar
-                tar xf $argv[1]
-            case *.tbz2
-                tar xjf $argv[1]
-            case *.tgz
-                tar xzf $argv[1]
-            case *.zip
-                unzip $argv[1]
-            case *.Z
-                uncompress $argv[1]
-            case *.7z
-                7z x $argv[1]
-            case *
-                echo "'$argv[1]' cannot be extracted via extract()"
-        end
-    else
-        echo "'$argv[1]' is not a valid file"
+    set ext zip rar bz2 gz tar tbz2 tgz Z 7z xz exe tar.bz2 tar.gz tar.xz lzma
+    if test -z "$argv"
+        # display usage if no parameters given
+        echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz|lzma>"
+        return
+    end
+    if not test -f "$argv"
+        echo $argv "- file does not exist"
+        return
+    end
+    switch $argv
+        case "*.$ext[1]"
+            unzip ./$argv
+        case "*.$ext[2]"
+            unrar x -ad ./$argv
+        case "*.$ext[3]"
+            bunzip2 ./$argv
+        case "*.$ext[4]"
+            gunzip ./$argv
+        case "*.$ext[5]"
+            tar xvf ./$argv
+        case "*.$ext[6]"
+            tar xvjf ./$argv
+        case "*.$ext[7]"
+            tar xvzf ./$argv
+        case "*.$ext[8]"
+            uncompress ./$argv
+        case "*.$ext[9]"
+            7z x ./$argv
+        case "*.$ext[10]"
+            unxz ./$argv
+        case "*.$ext[11]"
+            cabextract ./$argv
+        case "*.$ext[12]"
+            tar xvjf ./$argv
+        case "*.$ext[13]"
+            tar xvzf ./$argv
+        case "*.$ext[14]"
+            tar xvJf ./$argv
+        case "*.$ext[15]"
+            unlzma ./$argv
+        case '*'
+            echo "extract: $argv - unknown archive method"
     end
 end
-
 
 # starship
 set -x STARSHIP_CONFIG $HOME/.config/fish/starship.toml
