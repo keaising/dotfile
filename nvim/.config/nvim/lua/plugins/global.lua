c3f5c825dc0a8031b136484b13e312f5d1480d33
keaising <keaising@gmail.com>
2023-07-31 14:23:15 +0800

    revert autopair from cmp

--- nvim/.config/nvim/lua/plugins/global.lua
+++ nvim/.config/nvim/lua/plugins/global.lua
@@ -33,4 +33,29 @@ return {
         "Mr-LLLLL/vim-interestingwords",
         branch = "fix-next-item",
     },
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup({
+                check_ts = true,
+                ts_config = {
+                    lua = { "string", "source" },
+                    javascript = { "string", "template_string" },
+                    java = false,
+                },
+                disable_filetype = { "TelescopePrompt", "spectre_panel" },
+                fast_wrap = {
+                    map = "<M-e>",
+                    chars = { "{", "[", "(", '"', "'" },
+                    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+                    offset = 0, -- Offset from pattern match
+                    end_key = "$",
+                    keys = "qwertyuiopzxcvbnmasdfghjkl",
+                    check_comma = true,
+                    highlight = "PmenuSel",
+                    highlight_grey = "LineNr",
+                },
+            })
+        end,
+    },
 }
