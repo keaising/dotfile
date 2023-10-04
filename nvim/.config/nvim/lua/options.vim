fcfcfbc01a6b5fdbe929cadc65e7ca2e4fb55073
keaising <keaising@gmail.com>
2023-10-04 18:11:21 +0800

    update for indent

--- nvim/.config/nvim/lua/options.vim
+++ nvim/.config/nvim/lua/options.vim
@@ -91,10 +91,3 @@ set termguicolors
 set t_Co=256             " 允许256色
 let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
 let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-
-highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine
-highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine
-highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine
-highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine
-highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine
-highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -55,33 +55,29 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         lazy = false,
         config = function()
-            vim.opt.termguicolors = true
-            vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+            local highlight = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
+            }
 
-            vim.opt.list = true
-            vim.opt.listchars:append("tab:| ")
-            vim.opt.listchars:append("trail:.")
-            vim.opt.listchars:append("extends:>")
-            vim.opt.listchars:append("precedes:<")
-
-            vim.g.indent_blankline_filetype = { "yml", "yaml", "json", "lua", "python" }
-
-            require("indent_blankline").setup({
-                space_char_blankline = " ",
-                char_highlight_list = {
-                    "IndentBlanklineIndent1",
-                    "IndentBlanklineIndent2",
-                    "IndentBlanklineIndent3",
-                    "IndentBlanklineIndent4",
-                    "IndentBlanklineIndent5",
-                    "IndentBlanklineIndent6",
-                },
-            })
+            local hooks = require("ibl.hooks")
+            -- create the highlight groups in the highlight setup hook, so they are reset
+            -- every time the colorscheme changes
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
+            require("ibl").setup({ indent = { highlight = highlight } })
         end,
     },
     {
