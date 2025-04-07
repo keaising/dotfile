f8913a3f585665fe01873701f832767cecd514c3
keaising <keaising@gmail.com>
2025-04-07 23:20:40 +0200

    modern intent-blankline

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -65,34 +65,19 @@ return {
     },
     {
         "lukas-reineke/indent-blankline.nvim",
-        lazy = false,
-        commit = "9637670",
+        main = "ibl",
         config = function()
-            vim.opt.termguicolors = true
-            vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-            vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
-
             vim.opt.list = true
             vim.opt.listchars:append("tab:| ")
             vim.opt.listchars:append("trail:.")
             vim.opt.listchars:append("extends:>")
             vim.opt.listchars:append("precedes:<")
-
-            vim.g.indent_blankline_filetype = { "yml", "yaml", "json", "lua" }
-
-            require("indent_blankline").setup({
-                space_char_blankline = " ",
-                char_highlight_list = {
-                    "IndentBlanklineIndent1",
-                    "IndentBlanklineIndent2",
-                    "IndentBlanklineIndent3",
-                    "IndentBlanklineIndent4",
-                    "IndentBlanklineIndent5",
-                    "IndentBlanklineIndent6",
+            require("ibl").setup({
+                indent = {
+                    char = "▏",
+                },
+                scope = {
+                    show_start = false,
                 },
             })
         end,
@@ -153,6 +138,7 @@ return {
         config = function()
             require("circles").setup({
                 icons = { empty = "", filled = "", lsp_prefix = "" },
+                lsp = true,
             })
         end,
     },
