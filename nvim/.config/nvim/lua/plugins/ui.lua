ec77f403b2805f930aba0377a37cd09b712a775e
keaising <keaising@gmail.com>
2025-04-10 07:46:33 +0200

    replace lead char with whitespace

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -72,6 +72,8 @@ return {
             vim.opt.listchars:append("trail:.")
             vim.opt.listchars:append("extends:>")
             vim.opt.listchars:append("precedes:<")
+            vim.opt.listchars:append("lead: ")
+            vim.opt.listchars:append("space: ")
             require("ibl").setup({
                 indent = {
                     char = "▏",
