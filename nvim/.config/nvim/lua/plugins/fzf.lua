3083f01210cde17956e7e7ba3eef58168276f6f5
keaising <keaising@gmail.com>
2025-05-18 06:19:27 +0800

    replace telescope with fzf-lua

---
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,92 @@
+local rg_opts = table.concat({
+    -- "--column",
+    "--line-number",
+    "--no-heading",
+    "--color=always",
+    "--smart-case",
+    "--max-columns=4096",
+    "--hidden",
+    "-g '!{.git,node_modules}/'",
+    "--fixed-strings",
+    "--sort path",
+    -- "-e",
+}, " ")
+
+return {
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        keys = {
+            {
+                "<leader>s",
+                function()
+                    require("fzf-lua").files()
+                end,
+            },
+            {
+                "<leader>f",
+                function()
+                    require("fzf-lua").live_grep({
+                        rg_opts = rg_opts,
+                        -- previewer = false,
+                        multiline = 2,
+                    })
+                end,
+            },
+            {
+                "<C-f>",
+                function()
+                    require("fzf-lua").blines({
+                        previewer = false,
+                    })
+                end,
+            },
+            {
+                "<CR>",
+                function()
+                    require("fzf-lua").resume()
+                end,
+            },
+            {
+                "<leader>lo",
+                function()
+                    require("fzf-lua").oldfiles()
+                end,
+            },
+            {
+                "<leader>dd",
+                function()
+                    require("fzf-lua").diagnostics_document()
+                end,
+            },
+            {
+                "<leader>dw",
+                function()
+                    require("fzf-lua").diagnostics_workspace()
+                end,
+            },
+            {
+                "<leader>gw",
+                function()
+                    require("fzf-lua").grep_cword({ rg_opts = rg_opts })
+                end,
+            },
+        },
+        config = function()
+            local fzf = require("fzf-lua")
+            fzf.setup({
+                winopts = {
+                    row = 1,
+                    height = 0.62,
+                    width = 1.0,
+                    preview = {
+                        border = "single",
+                        horizontal = "right:45%",
+                    },
+                    border = "single",
+                },
+                fzf_opts = { ["--cycle"] = true },
+            })
+        end,
+    },
+}
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,51 @@
+-- begin 当年闲的蛋疼弄的
+local entry_display = require("telescope.pickers.entry_display")
+local displayer = entry_display.create({
+    separator = " ",
+    items = {
+        { width = 4 },
+        { width = nil },
+        { width = nil },
+        { width = nil },
+    },
+})
+local function trim_folder_prefix(s)
+    local prefix = vim.fn.expand("~/code/")
+    if vim.startswith(s, prefix) then
+        return string.sub(s, #prefix + 1)
+    end
+    return s
+end
+local function make_entry(entry)
+    local utils = require("telescope.utils")
+    return {
+        value = entry.filename,
+        display = function(item)
+            local colors = {
+                ["ERROR"] = "Red",
+                ["WARN"] = "Purple",
+                ["INFO"] = "Yellow",
+                ["HINT"] = "Blue",
+            }
+            return displayer({
+                { string.format("%4d", item.lnum), colors[entry.type] },
+                { trim_folder_prefix(utils.transform_path({}, item.filename)), "" },
+                {
+                    string.format("%s> %s", string.lower(string.sub(item.type, 1, 1)), item.text),
+                    colors[entry.type],
+                },
+            })
+        end,
+        filename = entry.filename,
+        ordinal = utils.transform_path({}, entry.filename) .. entry.type,
+        lnum = entry.lnum,
+        col = entry.col,
+        type = entry.type,
+        text = entry.text,
+    }
+end
+-- end
+
 return {
     {
         "nvim-telescope/telescope.nvim",
@@ -12,117 +60,54 @@ return {
         config = function()
             -- mappings
             local bufopts = { noremap = true, silent = true }
-            local utils = require("telescope.utils")
             local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>s", function()
-                builtin.find_files()
-            end, bufopts)
-            -- vim.keymap.set("n", "<C-s>", function()
+            -- vim.keymap.set("n", "<leader>s", function()
             --     builtin.find_files()
             -- end, bufopts)
-            vim.keymap.set("n", "<leader>f", function()
-                builtin.live_grep()
-            end, bufopts)
-            vim.keymap.set("n", "<C-f>", function()
-                builtin.live_grep({
-                    search_dirs = { "%:p" },
-                    sorting_strategy = "ascending",
-                })
-            end, bufopts)
-            vim.keymap.set("n", "<C-o>", function()
-                builtin.oldfiles({
-                    cwd_only = true,
-                    sorting_strategy = "ascending",
-                })
-            end, bufopts)
-            vim.keymap.set("n", "<CR>", function()
-                builtin.resume()
-            end, bufopts)
-            vim.keymap.set("n", "<leader>ld", function()
-                local entry_display = require("telescope.pickers.entry_display")
-                local displayer = entry_display.create({
-                    separator = " ",
-                    items = {
-                        { width = 4 },
-                        { width = nil },
-                        { width = nil },
-                        { width = nil },
-                    },
-                })
-                local function trim_folder_prefix(s)
-                    local prefix = vim.fn.expand("~/code/")
-                    if vim.startswith(s, prefix) then
-                        return string.sub(s, #prefix + 1)
-                    end
-                    return s
-                end
-                local function make_entry(entry)
-                    return {
-                        value = entry.filename,
-                        display = function(item)
-                            local colors = {
-                                ["ERROR"] = "Red",
-                                ["WARN"] = "Purple",
-                                ["INFO"] = "Yellow",
-                                ["HINT"] = "Blue",
-                            }
-                            return displayer({
-                                { string.format("%4d", item.lnum), colors[entry.type] },
-                                { trim_folder_prefix(utils.transform_path({}, item.filename)), "" },
-                                {
-                                    string.format("%s> %s", string.lower(string.sub(item.type, 1, 1)), item.text),
-                                    colors[entry.type],
-                                },
-                            })
-                        end,
-                        filename = entry.filename,
-                        ordinal = utils.transform_path({}, entry.filename) .. entry.type,
-                        lnum = entry.lnum,
-                        col = entry.col,
-                        type = entry.type,
-                        text = entry.text,
-                    }
-                end
-                builtin.diagnostics({
-                    entry_maker = make_entry,
-                })
-            end, bufopts)
-            vim.keymap.set("n", "<leader>lj", function()
-                builtin.jumplist({ show_line = false })
-            end, bufopts)
+            -- vim.keymap.set("n", "<leader>f", function()
+            --     builtin.live_grep()
+            -- end, bufopts)
+            -- vim.keymap.set("n", "<C-f>", function()
+            --     builtin.current_buffer_fuzzy_find()
+            -- end, bufopts)
+            -- vim.keymap.set("n", "<leader>lo", function()
+            --     builtin.oldfiles({
+            --         cwd_only = true,
+            --         sorting_strategy = "ascending",
+            --     })
+            -- end, bufopts)
+            -- vim.keymap.set("n", "<CR>", function()
+            --     builtin.resume()
+            -- end, bufopts)
+            -- vim.keymap.set("n", "<leader>ld", function()
+            --     builtin.diagnostics({
+            --         entry_maker = make_entry,
+            --     })
+            -- end, bufopts)
+            -- vim.keymap.set("n", "<leader>lj", function()
+            --     builtin.jumplist({ show_line = false })
+            -- end, bufopts)
 
-            vim.keymap.set("n", "<leader>lg", function()
-                local previewers = require("telescope.previewers")
-                local opts = { current_file = vim.fn.expand("%") }
-                builtin.git_bcommits({
-                    git_command = { "gitlog_ts" },
-                    previewer = {
-                        previewers.git_commit_diff_to_parent.new(opts),
-                        previewers.git_commit_message.new(opts),
-                        previewers.git_commit_diff_as_was.new(opts),
-                        previewers.git_commit_diff_to_head.new(opts),
-                    },
-                })
-            end, bufopts)
+            -- vim.keymap.set("n", "<leader>lg", function()
+            --     local previewers = require("telescope.previewers")
+            --     local opts = { current_file = vim.fn.expand("%") }
+            --     builtin.git_bcommits({
+            --         git_command = { "gitlog_ts" }, -- 这玩意至今还有bug没有修,还需要搭配脚本使用,也是闲的蛋疼弄的,
+            --         previewer = {
+            --             previewers.git_commit_diff_to_parent.new(opts),
+            --             previewers.git_commit_message.new(opts),
+            --             previewers.git_commit_diff_as_was.new(opts),
+            --             previewers.git_commit_diff_to_head.new(opts),
+            --         },
+            --     })
+            -- end, bufopts)
 
             -- settings
             local actions = require("telescope.actions")
-            local normal_layout = {
-                horizontal = {
-                    preview_width = 0.6,
-                },
-            }
-            local wide_layout = {
-                width = 0.95,
-                horizontal = {
-                    preview_width = 0.6,
-                },
-            }
             require("telescope").setup({
                 defaults = {
                     vimgrep_arguments = {
                         "rg",
-                        "--color=never",
                         "--no-heading",
                         "--with-filename",
                         "--line-number",
@@ -143,7 +128,19 @@ return {
                             ["<C-a>"] = actions.cycle_previewers_prev,
                         },
                     },
-                    layout_config = normal_layout,
+                    sorting_strategy = "ascending",
+                    layout_strategy = "bottom_pane",
+                    border = true,
+                    borderchars = {
+                        prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
+                        results = { " " },
+                        preview = { "─", " ", "─", "│", "┌", "─", "─", "└" },
+                    },
+                    layout_config = {
+                        width = 1.0,
+                        height = 0.6,
+                        preview_width = 0.45,
+                    },
                 },
                 pickers = {
                     find_files = {
@@ -151,7 +148,6 @@ return {
                             "rg",
                             "--sort=path",
                             "--files",
-                            "--color=never",
                             "--no-heading",
                             "--with-filename",
                             "--line-number",
@@ -159,26 +155,6 @@ return {
                             "--hidden",
                             "--smart-case",
                         },
-                    },
-                    lsp_references = {
-                        layout_config = normal_layout,
-                        -- initial_mode = "normal",
-                        sorting_strategy = "ascending",
-                    },
-                    jumplist = {
-                        layout_config = wide_layout,
-                        -- initial_mode = "normal",
-                        sorting_strategy = "ascending",
-                    },
-                    diagnostics = {
-                        layout_config = wide_layout,
-                        -- initial_mode = "normal",
-                        sorting_strategy = "ascending",
-                    },
-                    git_bcommits = {
-                        layout_config = wide_layout,
-                        -- initial_mode = "normal",
-                        sorting_strategy = "ascending",
                     },
                 },
                 extensions = {
