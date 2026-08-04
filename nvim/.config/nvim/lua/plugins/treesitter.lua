76c4236f72329d8327c69b1248b0ebc85b095049
keaising <keaising@gmail.com>
2026-08-04 13:17:19 +0100

    chore(nvim): migrate nvim-treesitter to main branch for neovim 0.12

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,35 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
+        build = ":TSUpdate",
         config = function()
-            ---@diagnostic disable: missing-fields
-            require("nvim-treesitter.configs").setup({
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        node_incremental = "v",
-                        node_decremental = "V",
-                    },
-                },
-                -- ensure_installed = "all", -- one of "all" or a list of languages
-                ensure_installed = {
-                    "go",
-                    "gomod",
-                    "python",
-                    "lua",
-                    "typescript",
-                    "javascript",
-                    "tsx",
-                    "yaml",
-                    "bash",
-                    "json",
-                    "toml",
-                    "markdown",
-                    "query",
-                    "comment", -- highlight for "TODO", "FIXME"
-                },
-                ignore_install = {}, -- List of parsers to ignore installing
-                highlight = {
-                    enable = true,
-                    disable = { "sql" }, -- list of language that will be disabled
-                },
+            require("nvim-treesitter").install({
+                "go",
+                "gomod",
+                "python",
+                "lua",
+                "typescript",
+                "javascript",
+                "tsx",
+                "yaml",
+                "bash",
+                "json",
+                "toml",
+                "markdown",
+                "markdown_inline",
+                "query",
+                "comment",
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "*",
+                callback = function()
+                    if vim.bo.filetype ~= "sql" then
+                        pcall(vim.treesitter.start)
+                    end
+                end,
             })
         end,
     },
--- nvim/.config/nvim/queries/typescript/textobjects.scm
+++
@@ -1,4 +0,0 @@
-;; extends
-
-(pair (property_identifier) @keyed_element)
-(pair (property_identifier) ":" (_) @keyed_element)
